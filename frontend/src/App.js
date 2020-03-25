31a37bde9792c3609bee7d6f09d1d9335445fcf1
cabral33 <lsc.hef@uea.edu.br>
2020-03-25 02:18:27 -0400

    back-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className= 'classe'>
+      <p>
+        <a className="link" href= 'https://www.github.com' target="_blank">Site para o meu github</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;

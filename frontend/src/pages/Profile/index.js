154e402b24317cbc55d93462aebe6f0312c6ab4a
cabral33 <lsc.hef@uea.edu.br>
2020-03-27 06:32:30 -0400

    front-end da aplicação

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import './styles.css'
 
 import api from '../../services/api'
 
 export default function Profile(){
     const [incidents, setIncidents] = useState([])
-
+    
+    const history = useHistory()
+    
     const ongId = localStorage.getItem('ongId')
     const ongName = localStorage.getItem('ongName')
+
+    
 
     useEffect(() => {
         api.get('profile', {
@@ -36,6 +40,12 @@ export default function Profile(){
         }
     }
 
+    function handleLogout() {
+        localStorage.clear()
+
+        history.push('/')
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -43,7 +53,7 @@ export default function Profile(){
               <span>Bem vinda, {ongName}</span>
               
             <Link className='button' to='/incidents/new' >Cadastrar novo caso</Link>
-            <button type='button'>
+            <button onClick={handleLogout} type='button'>
                 <FiPower size={18} color='#E02041' />
             </button>
             </header>
